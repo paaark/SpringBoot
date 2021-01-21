6218b9bac6bf00f85949fb4d83939adfc7cfe625
DESKTOP-H9O7O0G\Jong Hwan <blockdaon_park@naver.com>
2021-01-22 04:09:49 +0900

    Initial commit

---
+++ src/main/java/com/boot/webservice/web/RestWebController.java
@@ -0,0 +1,14 @@
+package com.boot.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestWebController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World..!";
+    }
+
+}
